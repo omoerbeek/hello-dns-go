497764a7c7f4b039a4046fe685933192ef9fafa4
Otto Moerbeek <otto@drijf.net>
2019-01-24 13:05:57 +0100

    Use canonical Name.String() as key

--- cmd/tres.go
+++ cmd/tres.go
@@ -25,15 +25,16 @@ import (
 )
 
 var (
-	hints = map[*tdns.Name]net.IP{
-		tdns.MakeName("a.root-servers.net"): net.ParseIP("198.41.0.4"),
-		tdns.MakeName("f.root-servers.net"): net.ParseIP("192.5.5.241"),
-		tdns.MakeName("k.root-servers.net"): net.ParseIP("193.0.14.129"),
+	hints = map[string]net.IP{
+		"a.root-servers.net": net.ParseIP("198.41.0.4"),
+		"f.root-servers.net": net.ParseIP("192.5.5.241"),
+		"k.root-servers.net": net.ParseIP("193.0.14.129"),
 	}
-	roots map[*tdns.Name][]net.IP = make(map[*tdns.Name][]net.IP)
+	roots map[string][]net.IP = make(map[string][]net.IP)
 )
 
 type RRec struct {
+	Name tdns.Name
 	Data tdns.RRGen
 }
 
@@ -41,20 +42,21 @@ func resolveName(name *tdns.Name, nsip *net.IP, typ tdns.Type) []RRec {
 	x := make([]RRec, 2)
 	a := tdns.AGen{net.ParseIP("1.2.3.4")}
 	aaaa := tdns.AAAAGen{net.ParseIP("::1")}
-	x[0] = RRec{&a}
-	x[1] = RRec{&aaaa}
+	x[0] = RRec{*name, &a}
+	x[1] = RRec{*name, &aaaa}
 	return x
 }
 
 func resolveHints() {
 	for name, ip := range hints {
-		rrecs := resolveName(name, &ip, tdns.NS)
+		rrecs := resolveName(tdns.MakeName(name), &ip, tdns.NS)
 		for _, rrec := range rrecs {
+			key := rrec.Name.String()
 			switch a := rrec.Data.(type) {
 			case *tdns.AGen: 
-				roots[name] = append(roots[name], a.IP)
+				roots[key] = append(roots[key], a.IP)
 			case *tdns.AAAAGen: 
-				roots[name] = append(roots[name], a.IP)
+				roots[key] = append(roots[key], a.IP)
 			}
 		}
 	}
